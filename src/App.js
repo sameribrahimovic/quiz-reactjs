715bf755dff7f7f99c4101652c79df33af36e53a
sameribrahimovic <68476870+sameribrahimovic@users.noreply.github.com>
2023-09-01 19:02:11 +0200

    Start quiz funcionality added. Display question component using useReduser

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 import "./App.css";
 
 const initialState = {
@@ -24,6 +25,11 @@ function reducer(state, action) {
       return {
         ...state,
         status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
       };
     default:
       throw new Error("Action Unknown!");
@@ -46,7 +52,10 @@ function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
