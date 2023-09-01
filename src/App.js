fe1807b6d31e386b7ed0ccd6444b02400909a0f5
sameribrahimovic <68476870+sameribrahimovic@users.noreply.github.com>
2023-09-01 17:57:34 +0200

    Loading Questions from API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import Header from "./Header";
 import Main from "./Main";
 import "./App.css";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  //loading, error, ready, active or finished state of app
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action Unknown!");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:5001/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
